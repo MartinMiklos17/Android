f25f7843090b32a94c7d9ebb2cea3bd1e1a5f5f2
MartinMiklos17 <35778155+MartinMiklos17@users.noreply.github.com>
2018-03-06 16:04:15 +0100

    Add files via upload

--- webviewapp/mainPage.java
+++ webviewapp/mainPage.java
@@ -1,11 +1,31 @@
 package com.example.martin.webviewapp;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 
 public class mainPage extends AppCompatActivity {
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (Uri.parse(url).getHost().equals("http://fogyniakarokkozosseg.hu/")) {
+                // This is my web site, so do not override; let my WebView load the page
+                return false;
+            }
+            // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+            return true;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +36,8 @@ public class mainPage extends AppCompatActivity {
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         myWebView.loadUrl("http://fogyniakarokkozosseg.hu/");
+        myWebView.setWebViewClient(new WebViewClient());
+
+
     }
 }
